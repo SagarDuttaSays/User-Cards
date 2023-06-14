e4e03071d8d7d128b85c314f6f3a5f20a1d724ae
Sagar Dutta <duttasagar086@gmail.com>
2023-06-14 19:26:48 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,48 +1,74 @@
-import React, { useEffect, useState } from 'react';
+import React, { Component } from 'react';
 import axios from 'axios';
 
-function App() {
-  const [users, setUsers] = useState([]);
-
-  useEffect(() => {
-    const fetchUsers = async () => {
-      try {
-        const response = await axios.get('https://dummyjson.com/users');
-        setUsers(response.data.users);
-      } catch (error) {
-        console.error('Error fetching users:', error);
-      }
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: []
     };
+  }
 
-    fetchUsers();
-  }, []);
+  componentDidMount() {
+    this.fetchUsers();
+  }
 
-  return (
-    <div>
-      <h1 className='text-center'>User Cards</h1>
-      <div className="card-container">
-        {users.map((user) => (
-          <div key={user.id}  className={"card text-white bg-dark mb-3 d-inline-block text-center"} style={{"max-width": "100rem"}}>
+  fetchUsers = async () => {
+    try {
+      const response = await axios.get('https://dummyjson.com/users');
+      this.setState({ users: response.data.users });
+    } catch (error) {
+      console.error('Error fetching users:', error);
+    }
+  };
+
+  toggleCardDetails = (userId) => {
+    this.setState((prevState) => ({
+      users: prevState.users.map((user) => {
+        if (user.id === userId) {
+          return { ...user, showDetails: !user.showDetails };
+        }
+        return user;
+      })
+    }));
+  };
+
+  render() {
+    const { users } = this.state;
+
+    return (
+      <div className='container'>
+        <h1 className={'text-center bg-primary bg-gradient'}>User Cards</h1>
+        <div className="card-container">
+          {users.map((user) => (
+            <div key={user.id}  className={"card text-white bg-dark mb-3 d-inline-block text-center "} style={{"max-width": "200rem"}}>
             <h4 class="card-title">{user.firstName} {user.lastName}</h4>
-            <h6 class="card-subtitle mb-2 text-muted">{user.age} {user.gender}</h6>
+            <h6 class="card-subtitle mb-2 text-white">{user.age} {user.gender}</h6>
             <img  width={"100px"} src={user.image} alt="Card image"></img>
-            <div className='card-body'>
-            <ul className={"list-group list-group-flush"}>
-            <li class="list-group-item">{user.email}</li>
-            <li class="list-group-item">{user.phone}</li>
-            <li class="list-group-item">{user.username}</li>
-            <li class="list-group-item">{user.birthDate}</li>
-            <li class="list-group-item">{user.address["city"]}</li>
-            <li class="list-group-item">{user.address["coordinates"]["lat"]} {user.address["coordinates"]["lng"]}</li>
-            <li class="list-group-item">{user.bank["cardExpire"]}</li>
-            <li class="list-group-item">{user.company["name"]}</li>
-            </ul>
+              {user.showDetails ? (
+                <div className='card-body'>
+                <ul className={"list-group list-group-flush"}>
+                <li class="list-group-item"><label>Email: </label><input type={'email'} className={'form-control'} value={user.email} readOnly/></li>
+                <li class="list-group-item"><label>Phone Number: </label><input type={'text'} value={user.phone} className={'form-control'} readOnly/></li>
+                <li class="list-group-item"><label>Username: </label><input type={'text'} className={'form-control'} value={user.username} readOnly/></li>
+                <li class="list-group-item"><label>Date Of Birth: </label><input type={'date'} className={'form-control'} value={user.birthDate} readOnly/></li>
+                <li class="list-group-item"><label>City: </label><input type={'text'} className={'form-control'} value={user.address["city"]} readOnly/></li>
+                <li class="list-group-item"><label>Latitude: </label><input type={'number'} className={'form-control'} value={user.address["coordinates"]["lat"]} readOnly/></li>
+                <li class="list-group-item"><label>Longitude: </label><input type={'number'} className={'form-control'} value={user.address["coordinates"]["lng"]} readOnly/></li>
+                <li class="list-group-item"><label>Card Expire Date: </label><input type={'text'} className={'form-control'} value={user.bank["cardExpire"]} readOnly/></li>
+                <li class="list-group-item"><label>Company: </label><input type={'text'} className={'form-control'} value={user.company["name"]} readOnly/></li>
+                </ul><br/>
+            <button onClick={() => this.toggleCardDetails(user.id)} className={'btn btn-primary'}>Show Less</button>
+                </div>
+              ) : (
+                <button onClick={() => this.toggleCardDetails(user.id)} className={'btn btn-primary'}>Show More</button>
+              )}
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
